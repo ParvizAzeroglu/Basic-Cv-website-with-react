a0c78dabe9e8110f561b822328b7d8fc6ba3fbd6
Parviz Azeroglu <azerogluparviz@gmail.com>
2023-10-29 16:31:28 +0400

    commit

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,17 @@
+export default function Data() {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">
+        Lorem Ipsum <span className="home__hand">👋</span>
+      </h1>
+      <h3 className="home__subtitle">Lorem</h3>
+      <p className="home__description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
+        aperiam neque reprehenderit vero eos necessitatibus?
+      </p>
+      <a href="" className="button button-flex">
+        Lorem, ipsum dolor. 👈
+      </a>
+    </div>
+  );
+}
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+// import "./home.css";
+
+export default function Social() {
+  return (
+    <div className="home__social">
+      <a href="" className="home__social-icon" target="_blank">
+        <i className="bx bxl-instagram"></i>
+      </a>
+      <a href="" className="home__social-icon" target="_blank">
+        <i className="bx bxl-twitter"></i>
+      </a>
+      <a href="" className="home__social-icon" target="_blank">
+        <i className="bx bxl-linkedin-square"></i>
+      </a>
+    </div>
+  );
+}
